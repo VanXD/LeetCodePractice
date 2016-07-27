9bd9e75876c290a8ba820c2fbafbf55cf95facab
vanxd <forever8187520@vip.qq.com>
2016-07-27 10:46:48 +0800

    注释写思路

--- src/main/java/com/vanxd/LengthOfLongestSubstring.java
+++ src/main/java/com/vanxd/LengthOfLongestSubstring.java
@@ -15,9 +15,17 @@ public class LengthOfLongestSubstring {
     public static void main(String[] args) {
 //        System.out.println(new LengthOfLongestSubstring().lengthOfLongestSubstring("dvdf"));
 //        System.out.println(new LengthOfLongestSubstring().lengthOfLongestSubstring("anviaj"));
-        System.out.println(new LengthOfLongestSubstring().lengthOfLongestSubstring("uqinntq"));
+//        System.out.println(new LengthOfLongestSubstring().lengthOfLongestSubstring("uqinntq"));
+        System.out.println(new LengthOfLongestSubstring().lengthOfLongestSubstring("abababcd"));
     }
 
+    /**
+     * 思路：有点桶排序的感觉？
+     *    将字符串里的字符转成char,其实就是个int值，再将该字符的序号放在桶里，
+     * 之后如果有重复的，则将该序号记录到j,j就是记录最长的重复序号,最后用i - j 得到他们的距离（字符串长度-最长的重复=最长的不重复子字符串）。
+     * @param s
+     * @return
+     */
     public int lengthOfLongestSubstring(String s) {
         if (s.length() == 0) return 0;
         int i = 0, j = 0, ret = 0;
